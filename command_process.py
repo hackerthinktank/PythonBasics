0253bc5a4b5b3dbcdd2c92a6eedb1ca06100127e
hackerthinktank <84163807+hackerthinktank@users.noreply.github.com>
2021-06-12 13:03:46 -0400

    Update command_process.py

--- command_process.py
+++ command_process.py
@@ -10,19 +10,25 @@ command_input=input("Command to run\n")
 
 
 """
-command_input.split() -- will break apart the command_input into a list for subprocess.run() using spaces as delimiters
-subprocess.run('ls','-l', '/', capture_output=True) would list the root directory and return the output
-str -- converts the output to a string, a little redundant but whatever
+  command_input.split()
+    -will break apart the command_input into a list for subprocess.run() using spaces as delimiters
+  subprocess.run('ls','-l', '/', capture_output=True) -
+    -would list the root directory and return the output
 """
 
 process=(str(subprocess.run(command_input.split(), capture_output=True)))
 
 """
-left="stdout=b\'" -- this is the left boundary of stdout output from subprocess.run()
-right="\', stderr=b" -- this is the right boundary of stdout output from subprocess.run()
-pre_return=(str(process[process.index(left)+len(left):process.index(right)])) -- this extracts the stdout from using the limits
-clean_return=pre_return.replace('\\n','\n').replace('\\t','\t') --- this fixes the newline characters from our output
-print(clean_return) -- gives us the clean return
+  left="stdout=b\'"
+    -this is the left boundary of stdout output from subprocess.run()
+  right="\', stderr=b" 
+    -this is the right boundary of stdout output from subprocess.run()
+  pre_return=(str(process[process.index(left)+len(left):process.index(right)])) 
+    -this extracts the stdout from using the limits left and right
+  clean_return=pre_return.replace('\\n','\n').replace('\\t','\t') 
+    -this fixes the newline characters from our output
+  print(clean_return)
+    -prints the clean return
 """
 
 left="stdout=b\'"
@@ -31,6 +37,6 @@ pre_return=(str(process[process.index(left)+len(left):process.index(right)]))
 clean_return=pre_return.replace('\\n','\n').replace('\\t','\t')
 print(clean_return)
 
-
+#Thats it -- pretty basic
 
 
