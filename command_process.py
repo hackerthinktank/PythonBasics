f383cc71a5d0feb082feaac025eb8f33b25a9e23
hackerthinktank <84163807+hackerthinktank@users.noreply.github.com>
2021-06-12 12:59:47 -0400

    Add files via upload

--- command_process.py
+++ command_process.py
@@ -2,13 +2,31 @@
 import subprocess 
 import sys
 import os
-import re
-import copy
+
+#Example python code for executing commands and returning clean stdout
+
+#Take input and stuff in command input
+command_input=input("Command to run\n")
+
+
+"""
+command_input.split() -- will break apart the command_input into a list for subprocess.run() using spaces as delimiters
+subprocess.run('ls','-l', '/', capture_output=True) would list the root directory and return the output
+str -- converts the output to a string, a little redundant but whatever
+"""
+
+process=(str(subprocess.run(command_input.split(), capture_output=True)))
+
+"""
+left="stdout=b\'" -- this is the left boundary of stdout output from subprocess.run()
+right="\', stderr=b" -- this is the right boundary of stdout output from subprocess.run()
+pre_return=(str(process[process.index(left)+len(left):process.index(right)])) -- this extracts the stdout from using the limits
+clean_return=pre_return.replace('\\n','\n').replace('\\t','\t') --- this fixes the newline characters from our output
+print(clean_return) -- gives us the clean return
+"""
 
 left="stdout=b\'"
 right="\', stderr=b"
-command_input=input("Command to run\n")
-process=(str(subprocess.run(command_input.split(), capture_output=True)))
 pre_return=(str(process[process.index(left)+len(left):process.index(right)]))
 clean_return=pre_return.replace('\\n','\n').replace('\\t','\t')
 print(clean_return)
