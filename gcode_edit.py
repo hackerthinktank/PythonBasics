5bff0dbfa86751ca5fac1684e97b49287a9ac1a0
hackerthinktank <84163807+hackerthinktank@users.noreply.github.com>
2021-07-30 14:13:30 -0400

    Update gcode_edit.py

--- gcode_edit.py
+++ gcode_edit.py
@@ -4,13 +4,14 @@ import re
 import sys
 import fileinput
 
-"Usage gcode_edit.py gcodeprogram.gcode axis offset -- gcode_edit.py X -25"
+"Usage gcode_edit.py gcodeprogram.gcode axis offset -- gcode_edit.py program.gcode X -25"
 with fileinput.FileInput(sys.argv[1], inplace=True, backup='.bak') as file:
     for line in file:
         elements = re.split(' ', line)
         for i in range(len(elements)):
             if (elements[i].find(str(sys.argv[2])) != -1):
                 axis_integer=(float((elements[i])[1:])-float(sys.argv[3]))
+                "This assumes that your orgin is the lower left of the table. If it is the center of the table comment out these two lines."
                 if (axis_integer < 0):
                     axis_integer=0
                 elements[i]=(str(sys.argv[2])+str(axis_integer))
