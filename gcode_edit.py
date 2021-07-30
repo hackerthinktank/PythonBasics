355fe25ef33f7990ab260717a117c29de7cdd97d
hackerthinktank <84163807+hackerthinktank@users.noreply.github.com>
2021-07-30 14:18:03 -0400

    Update gcode_edit.py

--- gcode_edit.py
+++ gcode_edit.py
@@ -10,7 +10,7 @@ with fileinput.FileInput(sys.argv[1], inplace=True, backup='.bak') as file:
         elements = re.split(' ', line)
         for i in range(len(elements)):
             if (elements[i].find(str(sys.argv[2])) != -1):
-                axis_integer=(float((elements[i])[1:])-float(sys.argv[3]))
+                axis_integer=round(float((elements[i])[1:])-float(sys.argv[3])),4)
                 "This assumes that your orgin is the lower left of the table. If it is the center of the table comment out these two lines."
                 if (axis_integer < 0):
                     axis_integer=0
